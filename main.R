af58a506d092560f01bf00c61c19ea18ef30c272
Freddy Drennan <drennanfreddy@gmail.com>
2020-11-08 16:40:22 -0700

    update

---
+++ main.R
@@ -0,0 +1,24 @@
+library(fs)
+library(readr)
+library(dplyr)
+library(skimr)
+
+remote_path <- 'https://github.com/fdrennan/covid-19-data/raw/master/public/data/owid-covid-data.csv'
+local_path <- 'covid-19.csv'
+
+if(!file_exists(local_path)) {
+  download.file(remote_path, local_path)
+}
+
+covid <-
+  read_csv(local_path)
+
+covid <-
+  covid %>%
+  select(-contains('total'))
+
+glimpse(covid)
+
+covid %>%
+  group_by(continent, location) %>%
+  count()
