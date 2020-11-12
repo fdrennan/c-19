d63a4003cd6c980e4737b718cadc8c88ad054615
Freddy Drennan <drennanfreddy@gmail.com>
2020-11-11 20:07:55 -0700

    update

--- update.R
+++ update.R
@@ -1,4 +1,3 @@
-library(glue)
 library(fs)
 library(readr)
 library(dplyr)
@@ -7,7 +6,6 @@ library(stringr)
 library(DBI)
 library(covid)
 library(dbx)
-library(purrr)
 
 remote_path <- 'https://github.com/fdrennan/covid-19-data/raw/master/public/data/owid-covid-data.csv'
 local_path <- 'covid-19.csv'
@@ -22,31 +20,23 @@ covid <- read_csv(local_path)
 
 covid <-
   covid %>%
-  select(-contains('total')) %>% 
-  mutate(id = paste0(continent, ' - ', location))
+  select(-contains('total'))
 
+covid <-
+  covid %>%
+  mutate(key = paste0(iso_code, continent, location, date, collapse = '_'))
+
+covid <-
+  covid %>%
+  select(key, everything())
 
 con <- postgres_connector()
-
-if (TRUE) {
+if (FALSE) {
   if (dbExistsTable(conn = con, name = 'covid')) {
-    message('Deleting covid table')
     dbRemoveTable(conn = con, name = 'covid')
   }
-  dbCreateTable(conn = con, name = 'covid', fields = head(covid, 1000)) 
+  dbCreateTable(conn = con, name = 'covid', fields = covid)
 }
 
-
-covid_split <- split(
-  covid, paste0(covid$continent, covid$location)
-) 
-  
-walk(
-  covid_split,
-  function(x) {
-    continent <- unique(x$continent)
-    location <- unique(x$location)
-    message(glue('Uploading {continent} {location}'))
-    dbAppendTable(conn = con, name = 'covid', value = x, append=TRUE)
-})
-
+dbWriteTable(conn = con, name = 'covid', value = covid, overwrite=TRUE)
+dbxUpsert(con, "covid", covid, where_cols = 'key')
